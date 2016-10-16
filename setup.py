5d68bd7a89bd3a4ef9d91691bc6b84977c860f72
Krzysztof Magosa <krzysztof@magosa.pl>
2016-10-16 20:48:41 +0200

    version 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='ansible-toolset',
-    version='0.1.0',
+    version='0.1.1',
     description='Useful Ansible toolset',
     url='https://github.com/krzysztof-magosa/ansible-toolset',
     author='Krzysztof Magosa',
