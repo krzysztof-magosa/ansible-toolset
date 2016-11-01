7baec1db2dfd85faa94763c02c8937cd9a703c85
Krzysztof Magosa <krzysztof@magosa.pl>
2016-11-01 15:40:44 +0100

    use new style object

--- ansible_toolset/ansible/base.py
+++ ansible_toolset/ansible/base.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from ansible_toolset import utils
 
-class AnsibleAdapterBase:
+class AnsibleAdapterBase(object):
     def __init__(self, vault_password):
         self.vault_password = vault_password
         self.init()
